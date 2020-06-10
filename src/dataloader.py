3985da9c7639434d9a3729b4f952a90a4fdceab2
John Flynn <johnflynn90@gmail.com>
2020-06-10 16:18:23 +0200

    Add rescaling of input/color data

--- src/dataloader.py
+++ src/dataloader.py
@@ -3,8 +3,9 @@ import glob
 import torch
 import numpy as np
 #rom torchvision import io
-from skimage import io
+from skimage import io, transform
 from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
 
 #from torch.utils.data.dataloader import default_collate
 
@@ -12,9 +13,11 @@ _SCREEN_HEIGHT = 968
 _SCREEN_WIDTH = 1296
 _UV_CHANNELS = 2
 
+_INPUT_SIZE = 256 # Size of input data to the network
+
 class UVDataset(Dataset):
-    def __init__(self, data_dir, uv_color_filenames):
-        self.data_dir = data_dir
+    def __init__(self, uv_color_filenames, transform=None):
+        self.transform = transform
         self.uv_color_filenames = uv_color_filenames
         pass
 
@@ -36,9 +39,62 @@ class UVDataset(Dataset):
 
         sample = {'uv': uv_image, 'color': color_image}
 
-        # TODO: Transform input
+        if self.transform:
+            sample = self.transform(sample)
 
         return sample
+
+class Rescale(object):
+    """Rescale the image in a sample to a given size.
+
+    Args:
+        output_size (tuple or int): Desired output size. If tuple, output is
+            matched to output_size. If int, smaller of image edges is matched
+            to output_size keeping aspect ratio the same.
+    """
+
+    def __init__(self, output_size):
+        assert isinstance(output_size, (int, tuple))
+        self.output_size = output_size
+
+    def __call__(self, sample):
+        input_image, color_image = sample['uv'], sample['color']
+
+        h, w = color_image.shape[:2]
+
+        if isinstance(self.output_size, int):
+            if h > w:
+                new_h, new_w = self.output_size * h / w, self.output_size
+            else:
+                new_h, new_w = self.output_size, self.output_size * w / h
+        else:
+            new_h, new_w = self.output_size
+
+        new_h, new_w = int(new_h), int(new_w)
+
+        # Nearest Neighbor Resizing
+        input_image = transform.resize(input_image, (new_h, new_w), order=0)
+        color_image = transform.resize(color_image, (new_h, new_w), order=0)
+
+        return {'uv': input_image, 'color': color_image}
+
+
+class ToTensor(object):
+    """Convert ndarrays in sample to Tensors."""
+
+    def __call__(self, sample):
+        input_image, color_image = sample['uv'], sample['color']
+
+        # TODO: Is axis swapping necessary for uv coords?
+
+        # swap color axis because
+        # numpy image: H x W x C
+        # torch image: C X H X W
+        input_image = input_image.transpose((2, 0, 1))
+        color_image = color_image.transpose((2, 0, 1))
+        return {'uv': torch.from_numpy(input_image),
+                'color': torch.from_numpy(color_image)}
+
 
 class UVDataLoader(DataLoader):
     # TODO: Rewrite this class in a more understandable way
@@ -48,10 +104,14 @@ class UVDataLoader(DataLoader):
         input_color_filenames = self.load_input_color_filenames(data_dir)
         if training:
             train_filenames = self.generate_temporal_train_split(input_color_filenames, skip)
-            self.dataset = UVDataset(data_dir, train_filenames)
+            self.dataset = UVDataset(train_filenames, transform=transforms.Compose([
+                Rescale(_INPUT_SIZE),
+                ToTensor()]))
         else:
             val_filenames = self.generate_temporal_val_split(input_color_filenames, skip)
-            self.dataset = UVDataset(data_dir, val_filenames)
+            self.dataset = UVDataset(val_filenames, transform=transforms.Compose([
+                Rescale(_INPUT_SIZE),
+                ToTensor()]))
 
 
         self.init_kwargs = {
