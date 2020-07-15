6e36fce08119e40f06a2ac7226f629a1272a44c8
John Flynn <johnflynn90@gmail.com>
2020-07-16 00:32:00 +0200

    Revert "Reverse regularization of texture hierarchical layers"
    This reverts commit 8f485d623110647a1fe0b4551911481cbd9f6e85.

--- models/losses.py
+++ models/losses.py
@@ -8,8 +8,9 @@ def photometric_reproduction_loss(output, target):
 
 def laplacian_pyramid_l2_regularization(weights, lam):
     loss = 0
+    num_layers = len(weights)
     for i, layer in enumerate(weights):
-        lam_prime = lam * ((4 ** i) - 1)
+        lam_prime = lam * ((4 ** (num_layers - i - 1)) - 1)
         loss = loss + 0.5 * lam_prime * torch.norm(layer, 2)
 
     return loss
