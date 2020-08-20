4c1e91fc6e71efb9abecebc75955c500b56becbe
John Flynn <johnflynn90@gmail.com>
2020-08-20 20:06:44 +0200

    Add use bilinear interpolation for color and anti-aliasing for down sampling

--- data_loaders/scannet_render_loader.py
+++ data_loaders/scannet_render_loader.py
@@ -91,7 +91,10 @@ class Rescale(object):
         #  though could mean losing a bit of precision when converting from [0, 1] to [-1, 1].
         #  What is the right way to do this conversion?
         input_image = transform.resize(input_image, (new_h, new_w), order=0)
-        color_image = transform.resize(color_image, (new_h, new_w), order=0)
+
+        # Use bilinear interpolation for color image and anti-aliasing if resize
+        #  is downsampling
+        color_image = transform.resize(color_image, (new_h, new_w), order=1, anti_aliasing=(h > new_h))
 
         return {'uv': input_image, 'color': color_image}
 
