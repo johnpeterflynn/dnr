9d20d7b1cdd6177b2d97abf85d4e2f8215381dd8
Flynn <john.flynn@tum.de>
2020-09-17 11:22:41 +0200

    Allow cuda to make nondeterministic optimizations

--- train.py
+++ train.py
@@ -14,8 +14,10 @@ import subprocess
 # fix random seeds for reproducibility
 SEED = 123
 torch.manual_seed(SEED)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
+#torch.backends.cudnn.deterministic = True
+#torch.backends.cudnn.benchmark = False
+torch.backends.cudnn.benchmark = True
+torch.backends.cudnn.enabled = True
 np.random.seed(SEED)
 
 
