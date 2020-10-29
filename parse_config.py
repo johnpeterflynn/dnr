e4a2ecdd9d82d3f2ee2bf64dc7c0a9cc3a6aefd5
Flynn <john.flynn@tum.de>
2020-10-29 16:39:32 +0100

    Fix log_dir referenced during dry run

--- parse_config.py
+++ parse_config.py
@@ -47,8 +47,9 @@ class ConfigParser:
             # save updated config file to the checkpoint dir
             write_json(self.config, self.save_dir / 'config.json')
 
-        # configure logging module
-        setup_logging(self.log_dir)
+            # configure logging module
+            setup_logging(self.log_dir)
+        
         self.log_levels = {
             0: logging.WARNING,
             1: logging.INFO,
